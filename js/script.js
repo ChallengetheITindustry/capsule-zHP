e4d4b60ae6023c2ef9955b92608192093a8e8695
大西泰生 <cpz-user@ONISHI-MacBookPro-16-C02CPNR7MD6M.local>
2021-08-31 13:49:53 +0900

    各部屋にボタンを設定するように実装

--- js/script.js
+++ js/script.js
@@ -5,11 +5,15 @@
     const castleId = document.getElementById('castle-id');
     const backgroundImageId = document.getElementById('background-image-id');
     const body = document.querySelector('body');
+    const cloudWhite1 = document.getElementById('cloud-white1')
 
     window.addEventListener('click', () => {
         castleId.classList.add('castle');
         castleId.classList.remove('castle-still');
         body.classList.add('body-background-image');
         body.classList.remove('body-color');
+        cloudWhite1.classList.remove('cloud-white1-still');
+        cloudWhite1.classList.add('cloud-white1');
+
     })
 }
