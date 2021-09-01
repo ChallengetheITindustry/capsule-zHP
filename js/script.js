40c850cf7b6bf9761e94af9e3b3eead5dcd9d66d
大西泰生 <cpz-user@ONISHI-MacBookPro-16-C02CPNR7MD6M.local>
2021-09-01 16:09:54 +0900

    一回のみ関数が実行できるように実装

--- js/script.js
+++ js/script.js
@@ -3,18 +3,27 @@
 
 {
 
+    let clicked = false;
+
     document.getElementById("transparentBtn").style.display = "none";
     const castleId = document.getElementById('castle-id');
     const backgroundImageId = document.getElementById('background-image-id');
     const body = document.querySelector('body');
 
     window.addEventListener('click', () => {
+
+        if (clicked == true) {
+            console.log('💢すでにクリックしています💢')
+            return;
+        }
         castleId.classList.add('castle');
         castleId.classList.remove('castle-still');
         body.classList.add('body-background-image');
         body.classList.remove('body-color');
-
+        console.log('クラスの追加と削除を行いました🚀')
         setTimeout(transparentBtn, 4000);
+
+        clicked = true;
 
     })
 
