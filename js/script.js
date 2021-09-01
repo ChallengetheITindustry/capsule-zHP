f5a962058e16f66f50aabba9d70904e151284623
大西泰生 <cpz-user@ONISHI-MacBookPro-16-C02CPNR7MD6M.local>
2021-09-01 15:50:32 +0900

    クリックした時に時間差で透明ボタンが表示されるように設定

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 
 
 {
+
+    document.getElementById("transparentBtn").style.display = "none";
     const castleId = document.getElementById('castle-id');
     const backgroundImageId = document.getElementById('background-image-id');
     const body = document.querySelector('body');
@@ -11,5 +13,15 @@
         castleId.classList.remove('castle-still');
         body.classList.add('body-background-image');
         body.classList.remove('body-color');
+
+        setTimeout(transparentBtn, 4000);
+
     })
+
+    function transparentBtn() {
+        const transparentBtn = document.getElementById("transparentBtn");
+        transparentBtn.style.display = "block";
+        console.log('透明ボタンを追加しました🚀');
+    }
+
 }
