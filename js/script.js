7a6d1b505b70da556e2bd2060d3e3287273dcdc3
大西泰生 <cpz-user@ONISHI-MacBookPro-16-C02CPNR7MD6M.local>
2021-09-01 15:15:26 +0900

    修正

--- js/script.js
+++ js/script.js
@@ -5,16 +5,11 @@
     const castleId = document.getElementById('castle-id');
     const backgroundImageId = document.getElementById('background-image-id');
     const body = document.querySelector('body');
-    const cloudWhite1 = document.getElementById('cloud-white1')
-    const btnRoom = document.getElementsByClassName('btnRoom')
 
     window.addEventListener('click', () => {
         castleId.classList.add('castle');
         castleId.classList.remove('castle-still');
         body.classList.add('body-background-image');
         body.classList.remove('body-color');
-        cloudWhite1.classList.remove('cloud-white1-still');
-        cloudWhite1.classList.add('cloud-white1');
-        btnRoom.classList.remove('btnRoom');
     })
 }
